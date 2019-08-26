304c7b90a9594312708c5c57b49226dcc3a91fc5
Abhinav Sharma (abhi) <abhi18av@gmail.com>
2019-08-27 00:31:45 +0530

    render our first ever fulcro element

--- src/main/app/client.cljs
+++ src/main/app/client.cljs
@@ -2,21 +2,50 @@
   (:require
     [com.fulcrologic.fulcro.application :as app]
     [com.fulcrologic.fulcro.components :as comp :refer [defsc]]
-    [com.fulcrologic.fulcro.dom :as dom]))
+    [com.fulcrologic.fulcro.dom :as dom :refer [div p]]))
 
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defonce app (app/fulcro-app))
 
-(defsc Root [this props]
-  (dom/div "TODO"))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(defsc ComponentName
+       [this props] ; parameters. Available in body, and in *some* of the options
+       ; optional:  { ...options... }
+       (dom/div {:className "a" :id "id" :style {:color "red"}}
+                (dom/p "Hello")))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defsc Person [this {:person/keys [name age]}]
+       (dom/div
+         (dom/p "Name: " name)
+         (dom/p "Age: " age)))
+
+
+(def ui-person (comp/factory Person))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Main entry point for ReactJS
+(defsc Root [this props]
+       (dom/div
+         (ui-person {:person/name "Joe" :person/age 22})))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Main entry point for JS
 (defn ^:export init
   "Shadow-cljs sets this up to be our entry-point function. See shadow-cljs.edn `:init-fn` in the modules of the main build."
   []
   (app/mount! app Root "app")
   (js/console.log "Loaded"))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; These are for shadow-cljs
-
 (defn ^:export refresh
   "During development, shadow-cljs will call this on every hot reload of source. See shadow-cljs.edn"
   []
