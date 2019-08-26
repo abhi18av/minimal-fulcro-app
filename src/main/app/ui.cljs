f206ba328ca772d8861123fff2b8d8ac8de0845d
Abhinav Sharma (abhi) <abhi18av@gmail.com>
2019-08-27 01:23:13 +0530

    render friends and enemies

--- src/main/app/ui.cljs
+++ src/main/app/ui.cljs
@@ -6,17 +6,39 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Person Component
-(defsc Person [this {:person/keys [name age]}]
-  (dom/div
-    (dom/p "Name: " name)
-    (dom/p "Age: " age)))
 
-(def ui-person (comp/factory Person))
+(defsc Person [this {:person/keys [name age]}]
+       (dom/li
+         (dom/h5 (str name " (age: " age ")"))))
+
+;; The keyfn generates a react key for each element based on props.
+;; See React documentation on keys.
+(def ui-person (comp/factory Person {:keyfn :person/name}))
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; PersonList Component
+
+(defsc PersonList [this {:list/keys [label people]}]
+       (dom/div
+         (dom/h4 label)
+         (dom/ul
+           (map ui-person people))))
+
+(def ui-person-list (comp/factory PersonList))
 
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Main entry point for ReactJS
-(defsc Root [this props]
-  (dom/div
-    (ui-person {:person/name "Joe" :person/age 22})))
+(defsc Root [this {:keys [ui/react-key]}]
+       (let [ui-data {:friends {:list/label "Friends"
+                                :list/people [{:person/name "Sally" :person/age 32}
+                                             {:person/name "Joe" :person/age 22}]}
+                      :enemies {:list/label "Enemies"
+                                :list/people [{:person/name "Fred" :person/age 11}
+                                             {:person/name "Bobby" :person/age 55}]}}]
+         (dom/div
+           (ui-person-list (:friends ui-data))
+           (ui-person-list (:enemies ui-data)))))
