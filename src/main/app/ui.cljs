5005de1163d267e76374ce76a8e71fcfb5b6a2c0
Abhinav Sharma (abhi) <abhi18av@gmail.com>
2019-08-28 00:42:20 +0530

    add idents

--- src/main/app/ui.cljs
+++ src/main/app/ui.cljs
@@ -9,41 +9,41 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Person Component
-(defsc Person [this {:person/keys [name age]} {:keys [onDelete]}]
-       {:query         [:person/name :person/age]
-        :initial-state (fn [{:keys [name age] :as params}] {:person/name name :person/age age})}
+
+(defsc Person [this {:person/keys [name age] :as props} {:keys [onDelete]}]
+       {:query         [:person/id :person/name :person/age]
+        :ident         (fn [] [:person/id (:person/id props)])
+        :initial-state (fn [{:keys [id name age] :as params}] {:person/id id :person/name name :person/age age})}
        (dom/li
          (dom/h5 (str name " (age: " age ")") (dom/button {:onClick #(onDelete name)} "X"))))
-
 
 ;; The keyfn generates a react key for each element based on props.
 ;; See React documentation on keys.
 (def ui-person (comp/factory Person {:keyfn :person/name}))
 
 
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; PersonList Component
 
-(defsc PersonList [this {:list/keys [label people]}]
-       {:query [:list/label {:list/people (comp/get-query Person)}]
+(defsc PersonList [this {:list/keys [id label people] :as props}]
+       {:query [:list/id :list/label {:list/people (comp/get-query Person)}] ; (5)
+        :ident (fn [] [:list/id (:list/id props)])
         :initial-state
-               (fn [{:keys [label]}]
-                 {:list/label  label
-                  :list/people (if (= label "Friends")
-                                 [(comp/get-initial-state Person {:name "Sally" :age 32})
-                                  (comp/get-initial-state Person {:name "Joe" :age 22})]
-                                 [(comp/get-initial-state Person {:name "Fred" :age 11})
-                                  (comp/get-initial-state Person {:name "Bobby" :age 55})])})}
-       (let [delete-person (fn [name] (comp/transact! this [(api/delete-person {:list-name label :name name})]))]
+               (fn [{:keys [id label]}]
+                 {:list/id     id
+                  :list/label  label
+                  :list/people (if (= id :friends)
+                                 [(comp/get-initial-state Person {:id 1 :name "Sally" :age 32})
+                                  (comp/get-initial-state Person {:id 2 :name "Joe" :age 22})]
+                                 [(comp/get-initial-state Person {:id 3 :name "Fred" :age 11})
+                                  (comp/get-initial-state Person {:id 4 :name "Bobby" :age 55})])})}
+       (let [delete-person (fn [item-id] (comp/transact! this [(api/delete-person {:list id :item item-id})]))] ; (4)
          (dom/div
            (dom/h4 label)
            (dom/ul
              (map (fn [p] (ui-person (comp/computed p {:onDelete delete-person}))) people)))))
 
 (def ui-person-list (comp/factory PersonList))
-
-
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Main entry point for ReactJS
