5c0a3449e23370aa52553da79c91f16ceeb5dbeb
Abhinav Sharma (abhi) <abhi18av@gmail.com>
2019-08-30 02:21:33 +0530

    now we see only the list labels and not the people

--- src/main/app/ui.cljs
+++ src/main/app/ui.cljs
@@ -10,58 +10,45 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Person Component
 ;; TODO figure out where is the mutation broken - it is sensed by the fulcro inspector but doesn't reflect in the UI
-(defsc Person
-       "Documentation"
-       [this {:person/keys [id name age] :as props} {:keys [onDelete]}]
-       {:query         [:person/id :person/name :person/age]
-        :ident         (fn [] [:person/id (:person/id props)])
-        :initial-state (fn [{:keys [id name age] :as params}] {:person/id id :person/name name :person/age age})}
-       (dom/li
-         (dom/h5 (str name " (age: " age ")") (dom/button {:onClick #(onDelete id)} "X"))))
+(defsc Person [this {:person/keys [id name age] :as props} {:keys [onDelete]}]
+  {:query [:person/id :person/name :person/age]
+   :ident (fn [] [:person/id (:person/id props)])}
+  (dom/li
+    (dom/h5 (str name " (age: " age ")") (dom/button {:onClick #(onDelete id)} "X"))))
 
 ;; The keyfn generates a react key for each element based on props.
 ;; See React documentation on keys.
-(def ui-person (comp/factory Person {:keyfn :person/name}))
+(def ui-person (comp/factory Person {:keyfn :person/id}))
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; PersonList Component
 
-(defsc PersonList
-       "Documentation"
-       [this {:list/keys [id label people] :as props}]
-       {:query [:list/id :list/label {:list/people (comp/get-query Person)}]
-        :ident (fn [] [:list/id (:list/id props)])
-        :initial-state
-               (fn [{:keys [id label]}]
-                 {:list/id     id
-                  :list/label  label
-                  :list/people (if (= id :friends)
-                                 [(comp/get-initial-state Person {:id 1 :name "Sally" :age 32})
-                                  (comp/get-initial-state Person {:id 2 :name "Joe" :age 22})]
-                                 [(comp/get-initial-state Person {:id 3 :name "Fred" :age 11})
-                                  (comp/get-initial-state Person {:id 4 :name "Bobby" :age 55})])})}
-       (let [delete-person (fn [person-id] (comp/transact! this [(api/delete-person {:list id :person person-id})]))]
-         (dom/div
-           (dom/h4 label)
-           (dom/ul
-             (map (fn [p] (ui-person (comp/computed p {:onDelete delete-person}))) people)))))
+(defsc PersonList [this {:list/keys [id label people] :as props}]
+  {:query [:list/id :list/label {:list/people (comp/get-query Person)}]
+   :ident (fn [] [:list/id (:list/id props)])}
+  (let [delete-person (fn [person-id] (comp/transact! this [(api/delete-person {:list/id id :person/id person-id})]))] ; (2)
+    (dom/div
+      (dom/ul
+        (map #(ui-person (comp/computed % {:onDelete delete-person})) people)))))
+
 
 (def ui-person-list (comp/factory PersonList))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Main entry point for ReactJS
 
-(defsc Root
-       "Documentation"
-       [this {:keys [friends enemies]}]
-       {:query         [{:friends (comp/get-query PersonList)}
-                        {:enemies (comp/get-query PersonList)}]
-        :initial-state (fn [params] {:friends (comp/get-initial-state PersonList {:id :friends :label "Friends"})
-                                     :enemies (comp/get-initial-state PersonList {:id :enemies :label "Enemies"})})}
-       (dom/div
-         (ui-person-list friends)
-         (ui-person-list enemies)))
+(defsc Root [this {:keys [friends enemies]}]
+  {:query         [{:friends (comp/get-query PersonList)}
+                   {:enemies (comp/get-query PersonList)}]
+   :initial-state {}}
+  (dom/div
+    (dom/h3 "Friends")
+    (when friends
+      (ui-person-list friends))
+    (dom/h3 "Enemies")
+    (when enemies
+      (ui-person-list enemies))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
